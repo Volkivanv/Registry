9872a1de3bdf713f3fa51acd7d2bf756e5fc3ccc
Ivan Volkov <volkivanv@mail.ru>
2021-10-27 22:44:35 +0300

    Second commit

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 
 int main() {
-    std::map<int,std::string>patientList;
+    std::map<std::string,int>patientList;
 
     std::string request;
     std::cout << "Input your request, or 'exit' for exit" << std::endl;
@@ -13,16 +13,28 @@ int main() {
     int k = 0;
     while(request != "exit"){
         if(request == "Next"){
-            auto it = patientList.begin();
-            std::cout  << it -> second << std::endl;
-            patientList.erase(it);
+            if(patientList.size()>0) {
+                auto it = patientList.begin();
+                std::cout << it->first << std::endl;
+                if (it->second > 1) {
+                    it->second--;
+                } else {
+                    patientList.erase(it);
+                }
+            } else{
+                std::cout << "There is nobody!" << std::endl;
+            }
         }else{
-            patientList.insert(std::make_pair(k,request));
-            k++;
+            auto it = patientList.find(request);
+            if(it == patientList.end()) {
+                patientList.insert(std::make_pair(request, 1));
+            } else {
+                it -> second++;
+            }
         }
         std::cout << "Input your request, or 'exit' for exit" << std::endl;
         std::getline(std::cin,request);
     }
-    
+
     return 0;
 }
